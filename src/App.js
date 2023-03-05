5f5682f1ff9a3449415f62b59ad79d8e758954c5
PierrickRauby <pierrick.rauby@gadz.org>
2023-03-04 23:06:40 -0500

    testing DNS again

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-  <button type="button" class="btn btn-primary">Just a test</button>
+  <button type="button" class="btn btn-primary">Just another test</button>
         </p>
         <a
           className="App-link"
