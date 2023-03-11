70e72202937be6405ebf3b4427b999af1428a812
PierrickRauby <pierrick.rauby@gadz.org>
2023-03-11 13:09:57 -0500

    pretemplate

--- src/components/PierrickNavbar.js
+++ src/components/PierrickNavbar.js
@@ -4,10 +4,11 @@ import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import '../styles/navbar.css'
 
 function PierrickNavbar() {
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar  bg="dark" variant="dark" expand="lg">
       <Container fluid>
         <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
