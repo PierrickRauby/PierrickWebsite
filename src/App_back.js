5208f0429db4b5c2122c487a32ee243a2f6b7c00
PierrickRauby <pierrick.rauby@gadz.org>
2023-03-11 18:43:13 -0500

    getting template to work

---
+++ src/App_back.js
@@ -0,0 +1,19 @@
+import './App.css';
+import PierrickNavbar from './components/PierrickNavbar.js';
+import './custom.scss'
+// import 'bootstrap/dist/css/bootstrap.css';
+
+function App() {
+  return (
+    <div className="App">
+      <head>
+<title>Hello</title>
+      </head>
+<PierrickNavbar />
+      <body>
+      </body>
+    </div>
+  );
+}
+
+export default App;
