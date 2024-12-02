f2efb565faa6ed1aef41131ac52d181739678cee
PierrickRauby <pierrick.rauby@gadz.org>
2024-12-02 08:33:56 -0500

    updated node version and modules

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,11 +1,11 @@
 import React from "react"
-import Typed from "react-typed"
-import ParticlesBackground from "./ParticlesBackground";
+import { ReactTyped } from 'react-typed';
+// import ParticlesBackground from "./ParticlesBackground";
 
 function Intro(props) {
 
   const HeaderTitleTypeAnimation = React.memo((props) => {
-    return <Typed
+    return <ReactTyped
       className="intro-text-slider"
       strings={props.titles}
       typeSpeed={80}
@@ -21,17 +21,17 @@ function Intro(props) {
       <div className="intro-content display-table">
         <div className="table-cell">
           <div id="ParticleContainer" className="container">
-            <ParticlesBackground/> 
+            {/* <ParticlesBackground/>  */}
             <div id="ParticleText">
             <h1 className="intro-title mb-4">Hello, I am {props.info.firstName}</h1>
             <p className="intro-subtitle">
               <span className="text-slider-items"></span>
               <strong className="text-slider">
                 <HeaderTitleTypeAnimation titles={props.info.titles} />
-              </strong>
+              </strong> 
             </p>
             </div>
-          </div>
+          </div> 
         </div>
        </div>
     </div>
