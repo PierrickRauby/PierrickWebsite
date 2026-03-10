8456f675592826e87ca15480bc37f924bebd459b
Pierrick Rauby <pierrick.rauby@metalenz.com>
2026-03-10 14:43:17 -0400

    Remove deprecated node-sass and fix react-particles import

--- src/components/ParticlesBackground.js
+++ src/components/ParticlesBackground.js
@@ -1,5 +1,5 @@
 import { useCallback } from "react";
-import Particles from "react-particles";
+import Particles from "react-tsparticles";
 import { loadSlim } from "tsparticles-slim"; // if you are going to use `loadSlim`, install the "tsparticles-slim" package too.
 
 function ParticlesBackground(props){
